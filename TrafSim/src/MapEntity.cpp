fc6b55970063411dc2160759ac460f06b0bf2d5e
gyntera1 <artur.gynter@aalto.fi>
2019-10-20 00:18:42 +0300

    Some MapEntity functions implemented

--- TrafSim/src/MapEntity.cpp
+++ TrafSim/src/MapEntity.cpp
@@ -1,2 +1,17 @@
 #include "MapEntity.hpp"
 
+namespace TrafSim
+{
+
+MapEntity::MapEntity(float lon, float lat)
+    : loc_(lon, lat)
+{
+}
+
+void MapEntity::set_loc(float lon, float lat)
+{
+    loc_.set_lon(lon);
+    loc_.set_lat(lat);
+}
+
+}; // namespace TrafSim
--- TrafSim/src/MapEntity.hpp
+++ TrafSim/src/MapEntity.hpp
@@ -1,10 +1,19 @@
 #pragma once
 
+#include <osmium/osm/location.hpp>
+
+#include "Window.hpp"
+
 namespace TrafSim
 {
 class MapEntity
 {
 public:
+    MapEntity(float lon = 0, float lat = 0);
+    const osmium::Location& get_loc() const { return loc_; }
+    void set_loc(float lon, float lat);
+    virtual void draw(const Window& window_) const = 0;
 private:
+    osmium::Location loc_;
 };
 }; // namespace TrafSim
