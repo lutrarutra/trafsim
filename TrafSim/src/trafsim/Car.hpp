fbeb7e6ec97ace739a676db9d6bd926eb4dc22d0
lutrarutra <artur.gynter@hotmail.com>
2019-11-02 15:48:31 +0200

    Cars prev and target pos is stored as nodes

--- TrafSim/src/trafsim/Car.hpp
+++ TrafSim/src/trafsim/Car.hpp
@@ -2,6 +2,7 @@
 
 #include <SFML/Graphics.hpp>
 
+#include "trafsim/Node.hpp"
 
 namespace TrafSim
 {
@@ -13,6 +14,9 @@ public:
     virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 private:
     sf::RectangleShape m_rect;
+    // p node as previous node t node as target node, where the car is going to
+    // from p node to t node
+    std::shared_ptr<Node> m_pNode, m_tNode;
 };
 
 };
